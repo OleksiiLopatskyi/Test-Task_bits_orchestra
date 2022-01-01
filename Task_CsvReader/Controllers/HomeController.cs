ae7c9464381c8cd7795d4104fc45ddddb917472f
OleksiiLopatskyi <oleksiy.lopatskiy@gmail.com>
2022-01-01 17:44:37 +0200

    Added:
    1)Install client-side library "jquery.unobtrusive-ajax.min".
    2)Install Package jquery.unobtrusive-ajax.
    3)Upload file to controller by ajax post request.

--- Task_CsvReader/Controllers/HomeController.cs
+++ Task_CsvReader/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,12 @@ namespace Task_CsvReader.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> UploadCSV(IFormCollection form)
+        {
+            var uploadedFile = form.Files["csvFile"];
+            return Json(new {message="success"});
         }
 
         public IActionResult Privacy()
